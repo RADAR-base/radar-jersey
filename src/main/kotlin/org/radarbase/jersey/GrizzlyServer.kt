4119082da7d5b242319ef2ac95b6a7e1ec483151
Joris Borgdorff <github@blootsvoets.net>
2019-10-07 11:04:11 +0200

    Add grizzly server

---
+++ src/main/kotlin/org/radarbase/jersey/GrizzlyServer.kt
@@ -0,0 +1,45 @@
+package org.radarbase.jersey
+
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory
+import org.glassfish.jersey.server.ResourceConfig
+import org.slf4j.LoggerFactory
+import java.lang.IllegalStateException
+import java.net.URI
+
+class GrizzlyServer(private val baseUri: URI, resources: ResourceConfig) {
+    private val server = GrizzlyHttpServerFactory.createHttpServer(baseUri, resources)
+
+    private val shutdownHook = Thread(Runnable {
+        logger.info("Stopping server..")
+        server.shutdown()
+    }, "shutdownHook")
+
+
+    fun listen() {
+        // register shutdown hook
+        Runtime.getRuntime().addShutdownHook(shutdownHook)
+
+        try {
+            server.start()
+
+            logger.info(String.format("Jersey app started on %s.\nPress Ctrl+C to exit...",
+                    baseUri))
+            Thread.currentThread().join()
+        } catch (e: Exception) {
+            logger.error("Error starting server: {}", e.toString())
+        }
+    }
+
+    fun shutdown() {
+        try {
+            Runtime.getRuntime().removeShutdownHook(shutdownHook)
+        } catch (ex: IllegalStateException) {
+            // ignore
+        }
+        server.shutdown()
+    }
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(GrizzlyServer::class.java)
+    }
+}
