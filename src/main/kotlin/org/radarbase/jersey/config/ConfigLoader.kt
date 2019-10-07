3775bb62903e60df5094970d459482c8b2363888
Joris Borgdorff <github@blootsvoets.net>
2019-10-07 16:23:42 +0200

    Add Java-compatible config loader

--- src/main/kotlin/org/radarbase/jersey/config/ConfigLoader.kt
+++ src/main/kotlin/org/radarbase/jersey/config/ConfigLoader.kt
@@ -30,13 +30,7 @@ object ConfigLoader {
         return createResourceConfig(enhancerFactory.createEnhancers())
     }
 
-    /**
-     * Load a configuration from YAML file. The filename is searched in the current working
-     * directory. This exits with a usage information message if the file cannot be loaded.
-     *
-     * @throws IllegalArgumentException if a file matching configFileName cannot be found
-     */
-    inline fun <reified T> loadConfig(fileName: String, args: Array<String>): T {
+    fun <T> loadConfig(fileName: String, args: Array<String>, clazz: Class<T>): T {
         val configFileName = when {
             args.size == 1 -> args[0]
             Files.exists(Paths.get(fileName)) -> fileName
@@ -48,13 +42,22 @@ object ConfigLoader {
         logger.info("Reading configuration from ${configFile.absolutePath}")
         try {
             val mapper = ObjectMapper(YAMLFactory())
-            return mapper.readValue(configFile, T::class.java)
+            return mapper.readValue(configFile, clazz)
         } catch (ex: IOException) {
             logger.error("Usage: <command> [$fileName]")
             logger.error("Failed to read config file $configFile: ${ex.message}")
             exitProcess(1)
         }
     }
+
+    /**
+     * Load a configuration from YAML file. The filename is searched in the current working
+     * directory. This exits with a usage information message if the file cannot be loaded.
+     *
+     * @throws IllegalArgumentException if a file matching configFileName cannot be found
+     */
+    inline fun <reified T> loadConfig(fileName: String, args: Array<String>): T =
+        loadConfig(fileName, args, T::class.java)
 
     /**
      * Create a resourceConfig based on the provided resource enhancers. This method also disables
