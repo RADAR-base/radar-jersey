e79a93d8065a109640d91c293343b6cc161f2b52
Joris Borgdorff <joris@thehyve.nl>
2020-10-01 10:20:41 +0200

    Combine authorization config with environment variables

--- src/main/kotlin/org/radarbase/jersey/auth/AuthConfig.kt
+++ src/main/kotlin/org/radarbase/jersey/auth/AuthConfig.kt
@@ -29,7 +29,19 @@ data class AuthConfig(
         val jwtKeystoreAlias: String? = null,
         /** Key password for the key alias in the p12 keystore. */
         val jwtKeystorePassword: String? = null,
-        )
+) {
+    fun combineWithEnv(): AuthConfig {
+        var config = this;
+        val mpConfig = managementPortal.combineWithEnv()
+        if (mpConfig != managementPortal) {
+            config = config.copy(managementPortal = mpConfig)
+        }
+        System.getenv("AUTH_KEYSTORE_PASSWORD")?.let {
+            config = config.copy(jwtKeystorePassword = it)
+        }
+        return config
+    }
+}
 
 data class MPConfig(
         /** URL for the current service to find the ManagementPortal installation. */
@@ -43,10 +55,21 @@ data class MPConfig(
         /** Interval after which the list of subjects in a project should be refreshed (minutes). */
         val syncParticipantsIntervalMin: Long = 5,
 ) {
-        /** Interval after which the list of projects should be refreshed. */
-        @JsonIgnore
-        val syncProjectsInterval: Duration = Duration.ofMinutes(syncProjectsIntervalMin)
-        /** Interval after which the list of subjects in a project should be refreshed. */
-        @JsonIgnore
-        val syncParticipantsInterval: Duration = Duration.ofMinutes(syncParticipantsIntervalMin)
+    /** Interval after which the list of projects should be refreshed. */
+    @JsonIgnore
+    val syncProjectsInterval: Duration = Duration.ofMinutes(syncProjectsIntervalMin)
+    /** Interval after which the list of subjects in a project should be refreshed. */
+    @JsonIgnore
+    val syncParticipantsInterval: Duration = Duration.ofMinutes(syncParticipantsIntervalMin)
+
+    fun combineWithEnv(): MPConfig {
+        var config = this
+        System.getenv("MANAGEMENT_PORTAL_CLIENT_ID")?.let {
+            config = config.copy(clientId = it)
+        }
+        System.getenv("MANAGEMENT_PORTAL_CLIENT_SECRET")?.let {
+            config = config.copy(clientSecret = it)
+        }
+        return config
+    }
 }
