5b31a8c8c7f95427b8f6f886b6d723bd3eb688ee
Joris Borgdorff <joris@thehyve.nl>
2020-10-19 15:31:28 +0200

    Organize dependencies

--- src/main/kotlin/org/radarbase/jersey/util/CachedValue.kt
+++ src/main/kotlin/org/radarbase/jersey/util/CachedValue.kt
@@ -1,7 +1,6 @@
 package org.radarbase.jersey.util
 
 import java.time.Duration
-import java.time.Instant
 import java.util.concurrent.Semaphore
 import java.util.concurrent.locks.Lock
 import java.util.concurrent.locks.ReentrantReadWriteLock
