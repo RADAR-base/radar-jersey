870e542aceb9474952609b0ba25fc3a2edecb26a
Joris Borgdorff <joris@thehyve.nl>
2020-11-23 10:20:54 +0100

    Updated dokka version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,9 +5,10 @@ include("radar-jersey-hibernate")
 
 pluginManagement {
     val kotlinVersion: String by settings
+    val dokkaVersion: String by settings
     plugins {
         kotlin("jvm") version kotlinVersion
         id("com.jfrog.bintray") version "1.8.5"
-        id("org.jetbrains.dokka") version kotlinVersion
+        id("org.jetbrains.dokka") version dokkaVersion
     }
 }
