dce04df8393bc75316090e58d5e7b6c9a14b48e1
Joris Borgdorff <joris@thehyve.nl>
2023-10-16 13:23:27 +0200

    Bumped dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,3 +1,4 @@
+@Suppress("ConstPropertyName")
 object Versions {
     const val project = "0.11.0-SNAPSHOT"
     const val kotlin = "1.9.10"
@@ -11,12 +12,12 @@ object Versions {
     const val mockitoKotlin = "5.1.0"
 
     const val hk2 = "3.0.4"
-    const val managementPortal = "2.0.1-SNAPSHOT"
+    const val managementPortal = "2.1.0"
     const val radarCommons = "1.1.1"
     const val javaJwt = "4.4.0"
     const val jakartaWsRs = "3.1.0"
     const val jakartaAnnotation = "2.1.1"
-    const val jackson = "2.15.2"
+    const val jackson = "2.15.3"
     const val slf4j = "2.0.9"
     const val log4j2 = "2.20.0"
     const val jakartaXmlBind = "4.0.1"
@@ -26,13 +27,13 @@ object Versions {
     const val hibernateValidator = "8.0.1.Final"
     const val glassfishJakartaEl = "4.0.2"
     const val jakartaActivation = "2.1.2"
-    const val swagger = "2.2.16"
-    const val mustache = "0.9.10"
+    const val swagger = "2.2.17"
+    const val mustache = "0.9.11"
 
     const val hibernate = "6.3.1.Final"
-    const val liquibase = "4.23.2"
+    const val liquibase = "4.24.0"
     const val postgres = "42.6.0"
     const val h2 = "2.2.224"
 
-    const val wrapper = "8.3"
+    const val wrapper = "8.4"
 }
