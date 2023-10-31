3e01fd7c307d2c8bbeff60b408a898040446942e
Joris Borgdorff <joris@thehyve.nl>
2023-10-31 15:38:44 +0100

    Added asyncService.withContext and support runBlocking from non-request
    Added AsyncApplicationEventListener for helping transitioning from
    events to suspended events.

--- radar-jersey/src/main/kotlin/org/radarbase/jersey/service/ScopedAsyncCoroutineService.kt
+++ radar-jersey/src/main/kotlin/org/radarbase/jersey/service/ScopedAsyncCoroutineService.kt
@@ -13,10 +13,12 @@ import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.coroutines.withContext
 import org.glassfish.jersey.process.internal.RequestScope
 import org.radarbase.jersey.coroutines.CoroutineRequestContext
 import org.radarbase.jersey.coroutines.CoroutineRequestWrapper
 import org.radarbase.kotlin.coroutines.consumeFirst
+import org.slf4j.LoggerFactory
 import kotlin.time.Duration
 
 class ScopedAsyncCoroutineService(
@@ -51,6 +53,31 @@ class ScopedAsyncCoroutineService(
         }
     }
 
+    override suspend fun <T> withContext(name: String, block: suspend () -> T): T {
+        // continue in existing context
+        return if (currentCoroutineContext()[CoroutineRequestContext.Key] != null) {
+            block()
+        } else {
+            val scope = try {
+                requestScope.get()
+            } catch (ex: Exception) {
+                logger.debug("Cannot construct request scope inside withContext", ex)
+                null
+            }
+            val wrapper = CoroutineRequestWrapper(scope) {
+                timeout = null
+                location = name
+            }
+            try {
+                withContext(wrapper.coroutineContext) {
+                    block()
+                }
+            } finally {
+                wrapper.cancelRequest()
+            }
+        }
+    }
+
     override fun <T> runBlocking(
         timeout: Duration,
         block: suspend () -> T,
@@ -74,12 +101,16 @@ class ScopedAsyncCoroutineService(
         }
     }
 
-    private inline fun <V> withWrapper(timeout: Duration, block: CoroutineRequestWrapper.() -> V): V {
-        val wrapper = CoroutineRequestWrapper(
-            timeout,
-            requestScope.get(),
-            "${requestContext.get().method} ${uriInfo.get().path}",
-        )
+    private inline fun <V> withWrapper(timeout: Duration? = null, block: CoroutineRequestWrapper.() -> V): V {
+        val scope = requestScope.get()
+        val wrapper = CoroutineRequestWrapper(scope) { hasExistingScope ->
+            this.timeout = timeout
+            location = if (hasExistingScope) {
+                "${requestContext.get().method} ${uriInfo.get().path}"
+            } else {
+                "asyncCoroutine"
+            }
+        }
         return wrapper.block()
     }
 
@@ -102,5 +133,9 @@ class ScopedAsyncCoroutineService(
                 block(continuation)
             }
         }
+    }
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(ScopedAsyncCoroutineService::class.java)
     }
 }
