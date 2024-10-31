55d0847c256517f8884e025f238450214daf431b
pvannierop <pim@thehyve.nl>
2024-10-31 11:15:53 +0100

    Up management portal client version to 2.1.7

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -12,7 +12,7 @@ object Versions {
     const val mockitoKotlin = "5.2.1"
 
     const val hk2 = "3.0.5"
-    const val managementPortal = "2.1.6"
+    const val managementPortal = "2.1.7"
     const val radarCommons = "1.1.2"
     const val javaJwt = "4.4.0"
     const val jakartaWsRs = "3.1.0"
