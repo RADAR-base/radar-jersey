c1f07d821dce31752772e96e33bd2ca793bc1188
Pim van Nierop <pim@thehyve.nl>
2025-09-02 17:13:02 +0200

    Up project version to 0.12.4

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 @Suppress("ConstPropertyName")
 object Versions {
-    const val project = "0.12.3"
+    const val project = "0.12.4"
     const val kotlin = "1.9.22"
 
     const val java: Int = 17
