e16befa2768789d46d7c0db773a4a8f2c917447c
this-Aditya <adityamishra2217@gmail.com>
2025-11-07 20:26:39 +0530

    Update tests

--- radar-jersey/src/main/kotlin/org/radarbase/jersey/exception/entity/ErrorResponse.kt
+++ radar-jersey/src/main/kotlin/org/radarbase/jersey/exception/entity/ErrorResponse.kt
@@ -1,6 +1,9 @@
 package org.radarbase.jersey.exception.entity
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class ErrorResponse(
     val error: String,
+    @param:JsonProperty("error_description")
     val description: String,
 )
