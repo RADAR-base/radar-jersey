9f7ea0e8b2fda0ccb7a2ef8590eb9773d3f0ee1e
Pim van Nierop <pim@thehyve.nl>
2026-03-10 19:40:38 +0100

    Apply March 2026 security updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,15 +20,20 @@ subprojects {
     apply(plugin = "org.radarbase.radar-kotlin")
     apply(plugin = "org.radarbase.radar-publishing")
 
+    // --- Vulnerability fixes start ---
     dependencies {
         plugins.withType<JavaPlugin> {
             constraints {
                 add("implementation", rootProject.libs.jackson.bom) {
                     because("Force safe version of Jackson across all modules")
                 }
+                add("implementation", rootProject.libs.jackson.core) {
+                    because("Force safe version of Jackson across all modules")
+                }
             }
         }
     }
+    // --- Vulnerability fixes end ---
 
     radarKotlin {
         javaVersion.set(libs.versions.java.get().toInt())
